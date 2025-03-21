029fe231e80f0bf14663e5cfd8114f83a31c2b33
Celebdil <lego-T.A.K@web.de>
2025-03-21 20:50:42 +0100

    Disabled django debug mode for production environment

--- backend/settings.py
+++ backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e5l-7ead(smuuero&%ir5a%s&jmofh5u9)f$22)y3sl^-zqa%i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DjangoDebug') != 'False'
 
 ALLOWED_HOSTS = ["*"]
 
