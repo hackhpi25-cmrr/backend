19f92bdeb0f42763573c0adb29e256a62c082c89
Josef Valentin <josef.valentin2004@gmail.com>
2025-03-21 22:45:21 +0100

    basic serializers
    they just give the fields directly

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,64 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
-from .models import EnumType
+from .models import (
+    Parameter,
+    EnumType,
+    Logbook,
+    ParameterAnswer,
+    Treatment,
+    Suggestion,
+    BaselineQuestion,
+    Baseline
+)
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+        fields = ['first_name', 'last_name', 'email', 'is_staff', 'date_joined']
+
+class ParameterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Parameter
+        fields = ['id', 'user', 'name', 'type']
+
 
 class EnumTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = EnumType
-        fields = ['id', 'display', 'value']
+        fields = ['id', 'parameter', 'display', 'value']
+
+
+class LogbookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Logbook
+        fields = ['id', 'user', 'time']
+
+
+class ParameterAnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ParameterAnswer
+        fields = ['id', 'parameter', 'answer', 'normalised_answer', 'logbook_entry']
+
+
+class TreatmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Treatment
+        fields = ['id', 'name']
+
+
+class SuggestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Suggestion
+        fields = ['id', 'logbook_entry', 'user', 'treatment', 'perceived_effectiveness', 'effectiveness']
+
+
+class BaselineQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BaselineQuestion
+        fields = ['id', 'name']
+
+
+class BaselineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Baseline
+        fields = ['id', 'user', 'question', 'normalised_answer']
