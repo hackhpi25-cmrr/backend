0bc3f3c4f60c24509ee7732f79ba299e175fefea
Celebdil <lego-T.A.K@web.de>
2025-03-21 21:36:29 +0100

    Added simplejwt

--- backend/urls.py
+++ backend/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from api.views import UserViewSet
 
@@ -28,4 +32,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
